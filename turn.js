e5a144183edb2af37a6b4dee0bbdbe30638f606f
Sanadi <79256438+sanadiibnuahmad@users.noreply.github.com>
2024-07-19 21:02:53 +0700

    Create turn.js

---
+++ turn.js
@@ -0,0 +1,10 @@
+$("#flipbook").turn({
+  // Aktifkan efek akselerasi
+  acceleration: true,
+  // Durasi animasi flip
+  duration: 600,
+  // Tambahkan gradien pada halaman
+  gradients: true,
+  // Tingkat elevasi halaman
+  elevation: 5
+});
